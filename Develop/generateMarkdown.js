c8048d8062ebff77b5d898bb87f1623370905e14
RLCastaldy <rlcastaldy@gmail.com>
2024-02-01 19:42:43 -0600

    cleaned up code and added duplicate license functions for MIT still need to finish GNU

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,41 +1,49 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license==="Apache"){
-    return `![Static Badge](https://img.shields.io/badge/License-Apache2.0-brightgreen)
-    `
+    return `![Static Badge](https://img.shields.io/badge/License-Apache2.0-brightgreen)`
+  }
+
+  if(license==="MIT"){
+    return `![Static Badge](https://img.shields.io/badge/License-MIT%203.0-black)`
+  }
+
+  if(license==="GNU General Public License"){
+    return `![Static Badge](https://img.shields.io/badge/License-GNU%203.0-blue)`
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
-  
-  // use for MIT License ling
-  // https://choosealicense.com/licenses/mit/
+  if(license==="Apache"){
+    return `http://www.apache.org/licenses/`
+  }
 
+  if(license==="MIT"){
+    return `https://choosealicense.com/licenses/mit/`
+  }
 
+  if(license==="GNU General Public License"){
+    return `https://fsf.org/`
+  }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license==="Apache"){
     return `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
   }
+
   if(license==="MIT"){
     return `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
   }
+
   if(license==="GNU General Public License"){
     return `Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.`
   }
+
   if(license==="No License"){
     return `Repository Is Not Licensed`
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 ${renderLicenseBadge(data.license)}
@@ -51,7 +59,7 @@ ${data.description}
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
-- [Credits](#credits)
+- [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
 
@@ -67,6 +75,8 @@ ${data.usage}
   
 ${renderLicenseSection(data.license)}
 
+${renderLicenseLink(data.license)}
+
 ## Contributing
 
 ${data.contributions}
@@ -77,6 +87,9 @@ ${data.tests}
 
 ## Questions
 
+${data.username}
+
+${data.email}
 
 `;
 }
--- generatedReadME.md
+++ generatedReadME.md
@@ -1,6 +1,5 @@
 
-![Static Badge](https://img.shields.io/badge/License-Apache2.0-brightgreen)
-    
+![Static Badge](https://img.shields.io/badge/License-MIT%203.0-black)
 
 # h
 
@@ -13,7 +12,7 @@ h
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
-- [Credits](#credits)
+- [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
 
@@ -27,7 +26,9 @@ h
 
 ## License
   
-A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+https://choosealicense.com/licenses/mit/
 
 ## Contributing
 
@@ -38,5 +39,9 @@ h
 h
 
 ## Questions
+
+h
+
+h
 
 
